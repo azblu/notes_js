dc6a493d34fa270299875610785920afa7f73e2a
azblu <98330304+azblu@users.noreply.github.com>
2022-10-01 13:20:57 -0500

    Create re.js

---
+++ SCOPE/re.js
@@ -0,0 +1,21 @@
+var firstName;
+firstName = "Sammy";
+console.log(firstName);
+
+var lastName = "David"; //Reasignación
+lastName = "Ana";
+
+console.log(lastName);
+
+var secondName = "Juan"; //Redeclarar
+var secondName = "Luis";
+
+console.log(secondName);
+
+let fruit = "Mango";
+fruit = "Kiwi";
+console.log(fruit);
+
+//No se puede reasignar
+//let fruit = 'Banna';
+//console.log(fruit)
