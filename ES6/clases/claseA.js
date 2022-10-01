4c38d2450aae9860667039678e4c9594af363bc8
azblu <98330304+azblu@users.noreply.github.com>
2022-10-01 13:18:25 -0500

    Create claseA.js

---
+++ ES6/clases/claseA.js
@@ -0,0 +1,20 @@
+class Calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new Calculator();
+console.log(calc.sum(2, 2));
+
+//const {hello} = requiere('./modules.js')
+import { hello } from "./modules.js";
+
+console.log(hello());
