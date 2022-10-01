17ff370c475f976a09b68f947195477e196a1c64
azblu <98330304+azblu@users.noreply.github.com>
2022-10-01 13:19:17 -0500

    Create index.js

---
+++ ES7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 4, 5];
+
+if (numbers.includes(4)) {
+  console.log("yes include");
+} else {
+  console.log("No include");
+}
+
+let base = 4;
+let exponente = 3;
+let result = base ** exponente;
+
+console.log(result);
