1d0c5ccdfc156cdb85f22aa04dc44f62646f657d
azblu <98330304+azblu@users.noreply.github.com>
2022-10-01 13:16:41 -0500

    Create flowStructures.js

---
+++ ES6/flowStructures.js
@@ -0,0 +1,15 @@
+let day = 'L'
+
+switch(day){
+    case 'L':
+        console.log('Lunes')
+        break
+    case 'M':
+        console.log('Martes')
+        break
+    case 'I':
+        console.log('Miercoles')
+        break
+    default:
+        console.log('Otro dia')
+}
